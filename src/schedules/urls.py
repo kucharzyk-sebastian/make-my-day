99d72b16909248cd380fd370758bb297ae95e9dc
Sebastian Kucharzyk <kucharzyk.sebastian@gmail.com>
2021-03-25 18:54:15 +0100

    implement CRUD for schedule

--- src/schedules/urls.py
+++ src/schedules/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import ScheduleListView
+from .views import ScheduleCreateView, ScheduleDeleteView, ScheduleDetailView, ScheduleListView, ScheduleUpdateView
 
 app_name = 'schedules'
 urlpatterns = [
-    path('', ScheduleListView.as_view(), name='schedule_list')
+    path('', ScheduleListView.as_view(), name='schedule_list'),
+    path('<int:pk>/delete', ScheduleDeleteView.as_view(), name='schedule_delete'),
+    path('<int:pk>/edit', ScheduleUpdateView.as_view(), name='schedule_update'),
+    path('<int:pk>/', ScheduleDetailView.as_view(), name='schedule_detail'),
+    path('create/', ScheduleCreateView.as_view(), name='schedule_create')
 ]
--- src/schedules/views.py
+++ src/schedules/views.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+from django.urls import reverse
 
 from .models import Schedule
 
@@ -9,3 +10,41 @@ class ScheduleListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return self.model.objects.filter(author=self.request.user)
+
+
+class ScheduleCreateView(LoginRequiredMixin, CreateView):
+    model = Schedule
+    fields = ('name', )
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('schedules:schedule_detail', args=[self.object.pk])
+
+
+class ScheduleDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
+    model = Schedule
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
+
+
+class ScheduleUpdateView(LoginRequiredMixin, UpdateView):
+    model = Schedule
+    fields = ('name', )
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
+
+    def get_success_url(self):
+        return reverse('schedules:schedule_detail', args=[self.object.pk])
+
+
+class ScheduleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Schedule
+    success_url = '/'
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
