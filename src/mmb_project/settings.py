3d60702eff7a83fc28ac1134941dc6d4215c107f
Sebastian Kucharzyk <36233877+kucharzyk-sebastian@users.noreply.github.com>
2021-03-25 12:33:50 +0100

    Add features around logging (#5)
    There are a few changes connected with signing in introduced here:
    * a new Django app for account
    * CustomUser model with basic functionalities
    * Templates for logging in/out, changing/resetting password
    
    There was also a minor change to pylint which excludes autogenerated migration directories
    
    Closes #2

--- src/mmb_project/settings.py
+++ src/mmb_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -99,6 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
@@ -118,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = ''
+LOGOUT_REDIRECT_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
