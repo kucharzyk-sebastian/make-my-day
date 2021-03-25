c973ebd79eefb1bc6acfe8d58c93b3687bbd28d8
Sebastian Kucharzyk <36233877+kucharzyk-sebastian@users.noreply.github.com>
2021-03-25 12:45:38 +0100

    Add signing up (#6)
    This change introduces new forms and views for creating a user in the accounts app.
    There is also some minor change to CustomUser model, i.e. its string representation.

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,3 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     """ Easily extensible custom user model """
+
+    def __str__(self):
+        return str(self.username)
--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import include, path
 
+from .views import CustomUserCreateView
+
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    path('create/', CustomUserCreateView.as_view(), name='create')
 ]
