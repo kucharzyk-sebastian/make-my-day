d3a82f2f222e12558df4c13d7f148aa81d84464f
Sebastian Kucharzyk <kucharzyk.sebastian@gmail.com>
2021-03-25 13:29:14 +0100

    create django app for schedules

--- src/mmb_project/urls.py
+++ src/mmb_project/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('schedules/', include('schedules.urls')),
 ]
