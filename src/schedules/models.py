bb378ec388149744d46da6b2ed70cac1602df997
Sebastian Kucharzyk <kucharzyk.sebastian@gmail.com>
2021-03-25 20:59:17 +0100

    add model for schedule entries

--- src/schedules/models.py
+++ src/schedules/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Schedule(models.Model):
@@ -8,3 +9,27 @@ class Schedule(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+class ScheduleEntry(models.Model):
+    class Meta:
+        verbose_name_plural = "Schedule entries"
+
+    class DayInWeek(models.TextChoices):
+        MONDAY = 0, _('Monday')
+        TUESDAY = 1, _('Tuesday')
+        WEDNESDAY = 2, _('Wednesday')
+        THURSDAY = 3, _('Thursday')
+        FRIDAY = 4, _('Friday')
+        SATURDAY = 5, _('Saturday')
+        SUNDAY = 6, _('Sunday')
+
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    day = models.CharField(max_length=3, choices=DayInWeek.choices)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self):
+        return self.title
