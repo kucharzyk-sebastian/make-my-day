0968a6f0a2eda851e2df811b8ae80a9973ee0e2b
Sebastian Kucharzyk <kucharzyk.sebastian@gmail.com>
2021-03-25 12:41:12 +0100

    add signing up functionality

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,12 @@
-# from django.shortcuts import render
+from django.urls.base import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+
+from .models import CustomUser
+
+
+class CustomUserCreateView(CreateView):
+    model = CustomUser
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
