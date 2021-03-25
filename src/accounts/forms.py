6ce8de3201d35771a592c3be9a0c1d1119904d2d
Sebastian Kucharzyk <36233877+kucharzyk-sebastian@users.noreply.github.com>
2021-03-25 15:10:03 +0100

    add missing email address to sign up form (#9)
    fixes #8

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -6,4 +6,4 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name')
+        fields = UserCreationForm.Meta.fields + ('email', 'first_name', 'last_name')
