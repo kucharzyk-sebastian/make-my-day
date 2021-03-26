0aa0be9ad76892e102b11abc554ee70882aded89
Sebastian Kucharzyk <36233877+kucharzyk-sebastian@users.noreply.github.com>
2021-03-26 09:56:23 +0100

    Implement entries for schedule (#16)
    This change includes:
    * add model for schedule entries
    * disallow entries with overlapping time frames
    * add create, update, delete views for entries
    * display entries as a table in the schedule view
    
    Closes #15

--- src/schedules/admin.py
+++ src/schedules/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
-from .models import Schedule
+from .models import Schedule, ScheduleEntry
+
+
+class ScheduleEntryInline(admin.TabularInline):
+    model = ScheduleEntry
 
 
 class ScheduleAdmin(admin.ModelAdmin):
     list_display = ('name', 'author')
+    inlines = [ScheduleEntryInline]
+
+
+class ScheduleEntryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'day', 'start_time', 'end_time')
 
 admin.site.register(Schedule, ScheduleAdmin)
+admin.site.register(ScheduleEntry, ScheduleEntryAdmin)
--- src/schedules/models.py
+++ src/schedules/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Schedule(models.Model):
@@ -8,3 +10,48 @@ class Schedule(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+class ScheduleEntry(models.Model):
+    class Meta:
+        verbose_name_plural = "Schedule entries"
+        ordering = ["day", "start_time"]
+
+    class DayInWeek(models.TextChoices):
+        MONDAY = 0, _('Monday')
+        TUESDAY = 1, _('Tuesday')
+        WEDNESDAY = 2, _('Wednesday')
+        THURSDAY = 3, _('Thursday')
+        FRIDAY = 4, _('Friday')
+        SATURDAY = 5, _('Saturday')
+        SUNDAY = 6, _('Sunday')
+
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    day = models.CharField(max_length=3, choices=DayInWeek.choices)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self):
+        return self.title
+
+    def clean(self):
+        q = ScheduleEntry.objects.filter(
+            day__exact=self.day,
+            start_time__gte=self.start_time,
+            start_time__lt=self.end_time
+        )
+        if q.count() > 1 or (q.count() == 1 and q[0].pk != self.pk):
+            raise ValidationError({'start_time': _('You already have an entry in this time frame!!')})
+
+        q = ScheduleEntry.objects.filter(
+            day__exact=self.day,
+            end_time__gt=self.start_time,
+            end_time__lte=self.end_time
+        )
+        if q.count() > 1 or (q.count() == 1 and q[0].pk != self.pk):
+            raise ValidationError({'start_time': _('You already have an entry in this time frame!!')})
+
+        if self.start_time > self.end_time:
+            raise ValidationError({'end_time': _('End time cannot be before start time.')})
