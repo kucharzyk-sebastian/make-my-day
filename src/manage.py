bde57bb5227c48a6bc0f811612e73e0e4b1fd5d4
Sebastian Kucharzyk <36233877+kucharzyk-sebastian@users.noreply.github.com>
2021-03-28 11:47:26 +0200

    Rename project name from mmb to mmd (#20)
    The project name is intended to be an acronym from Make My Date, however, some directories were wrongly named as "mmb".
    
    Fixes #19

--- src/manage.py
+++ src/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mmb_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mmd_project.settings')
     try:
         from django.core.management import execute_from_command_line # pylint: disable=import-outside-toplevel
     except ImportError as exc:
